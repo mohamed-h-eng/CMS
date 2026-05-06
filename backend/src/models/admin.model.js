886f7a9ec842ba8e6e0d350ff7f6ac686c654a0e
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-05-06 14:45:37 +0300

    initial commit

---
+++ backend/src/models/admin.model.js
@@ -0,0 +1,28 @@
+﻿import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Admin = mongoose.model('Admin', adminSchema);
---
+++ backend/src/models/event.model.js
@@ -0,0 +1,35 @@
+﻿import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Event = mongoose.model('Event', eventSchema);
