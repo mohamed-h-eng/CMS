9c74c3baf9e086063a99a9b5d1abcff8f0584075
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-05-06 14:27:10 +0300

    initial

--- frontend/src/App.js
+++
@@ -1,6 +0,0 @@
-﻿import React from 'react';
-import RouterComponent from './router/RouterComponent';
-
-export default function App() {
-  return <RouterComponent />;
-}
--- frontend/src/api/user.api.js
+++
@@ -1,7 +0,0 @@
-﻿import axios from 'axios';
-import { API_BASE_URL } from '../constants';
-
-export async function fetchUsers() {
-  const response = await axios.get(`${API_BASE_URL}/users`);
-  return response.data;
-}
