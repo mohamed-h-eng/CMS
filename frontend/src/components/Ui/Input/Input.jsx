aaaf0d08c9887dbee6ef04bde22bdc2ed8132b9e
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-05-18 09:58:41 +0300

    fix: conflicts in dashboard

--- frontend/src/components/Ui/Input/Input.jsx
+++ frontend/src/components/Ui/Input/Input.jsx
@@ -1,60 +1,11 @@
 import styles from './Input.module.css'
-<<<<<<< HEAD
-import { CiLocationOn } from "react-icons/ci";
-
-export function FormInput({title, placeholder}){
-    return (
-    <>
-=======
 
 export function Input({title, placeholder}){
     return (<>
->>>>>>> master
         <div className={styles.container}>
             <p className={styles.label}>{title}</p>
             <input className={styles.input} placeholder={placeholder}/>
         </div>
-<<<<<<< HEAD
-    </>
-    )
-}
-
-export function FormIconInput({icon, title, placeholder}){
-    return (
-    <>
-    <div className={styles.iconContainer}>
-        <p className={styles.label}>{title}</p>
-        <div className={styles.iconInputContainer}>
-            {/* <{icon}/> */}
-            <input className={styles.iconInput} placeholder={placeholder}/>
-        </div>
-    </div>
-    </>
-    )
-}
-
-export function FormDualLayout({children}){
-    return (
-    <>
-        <div className={styles.dualContainer}>
-            {children}
-        </div>
-    </>
-    )
-}
-
-export function FormImage({title}){
-    return(
-        <>
-            <div className={styles.container}>
-                <p className={styles.label}>{title}</p>
-                <div className={styles.dropArea}>
-
-                </div>
-            </div>
-        </>
-    )
-=======
     </>)
 }
 
@@ -64,5 +15,4 @@ export function InputDual({children}){
             {children}
         </div>
     </>)
->>>>>>> master
 }
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-import {FormInput, FormIconInput, FormDualLayout, FormImage} from '../../components/Ui/Input/Input'
-import Button from '../../components/Ui/Button/Button'
-import Layout from '../../components/Ui/Layout/Layout'
-import { CiLocationOn } from "react-icons/ci";
-export default function Dashboard(){
-    return(<>
-        <div className="container">
-            <Layout title="Event Title">
-                <FormInput title="Event Title"  placeholder="Enter title"/>
-                <FormDualLayout>
-                    <FormInput title="Category" placeholder="Enter Category"/>
-                    <FormInput title="Capacity" placeholder="Enter Capacity"/>    
-                </FormDualLayout>
-                <FormInput title="Date & Time" placeholder="Enter Date"/>
-                {/* <FormInput title="Venue Location" placeholder="Search Venue..."/> */}
-                <FormIconInput title="Venue Location" placeholder="Search Venue..." icon={CiLocationOn}/>
-                <FormInput title="Ticket Price ($)" placeholder="0.00"/>
-                <FormImage title="Event Image"/>
-                <Button>Publish Event</Button>
-            </Layout>
-        </div>
-    </>)
-}
-=======
 import SideBar from '../../components/Dashboard/Sidebar/Sidebar'
 import styles from './Dashboard.module.css'
 import Status from '../../components/Dashboard/Status/Status'
@@ -65,4 +40,3 @@ export default function Dashboard() {
     </div>
   )
 }
->>>>>>> master
